29e40052f64cb84f9fa44d9d68b1b2631205c568
Srikar <srikar.y.12@gmail.com>
2024-04-29 12:05:46 +0530

    added crores and lakhs display logic

--- src/LandCard.js
+++ src/LandCard.js
@@ -1,16 +1,32 @@
 import "bootstrap/dist/css/bootstrap.css";
 import Carousel from "react-bootstrap/Carousel";
 import "./LandCard.css";
+import { useState, useEffect } from "react";
+
 function LandCard({ land }) {
   const {
     land_media,
     total_price,
+    price_per_acre_crore,
     village_name,
     mandal_name,
     district_name,
     total_land_size_in_acres: { acres, guntas },
   } = land;
+  const [displayLakh, setDisplayLakh] = useState(false)
   const roundedPrice = parseFloat(total_price).toFixed(2);
+  const priceperacreinLakh = price_per_acre_crore.lakh;
+  const priceperacreincrore = price_per_acre_crore.crore;
+  const roundedPrice_per_acre =  parseFloat(priceperacreinLakh) + parseFloat(priceperacreincrore);
+
+  useEffect(() => {
+    // Set displayLakh based on pricePerAcreInCrore value
+    if (priceperacreincrore <= 0) {
+      setDisplayLakh(true);
+    } else {
+      setDisplayLakh(false);
+    }
+  }, [priceperacreincrore]);
   const imageStyle = {
     height: "220px",
     width: "100%",
@@ -39,20 +55,17 @@ function LandCard({ land }) {
           <>
             {acres > 0 && guntas === 0 && (
               <p>
-                <b>{acres} Acres</b> ₹ {roundedPrice} Crore
+                <b>{acres} Acres • </b> ₹ {roundedPrice_per_acre} {displayLakh ? "Lakhs" : "Crores"} per Acre
               </p>
             )}
             {acres > 0 && guntas !== 0 && (
               <p>
-                <b>
-                  {acres} Acres {guntas} Guntas
-                </b>{" "}
-                ₹ {total_price} Crore
+                <b> {acres} Acres {guntas} Guntas • </b> ₹ {roundedPrice_per_acre} {displayLakh ? "Lakhs" : "Crores"} per Acre
               </p>
             )}
             {acres === 0 && guntas > 0 && (
               <p>
-                <b>{guntas} Guntas</b> ₹ {roundedPrice} Crore
+                <b>{guntas} Guntas • </b> ₹ {roundedPrice} {displayLakh ? "Lakhs" : "Crores"} per Acre
               </p>
             )}
           </>
